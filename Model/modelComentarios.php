7347d281258d6a41f8b70cfe0d1b4785bb9aa4ee
Kevin Garces Hoyberg <kevin.garceshoyberg@gmail.com>
2021-07-14 01:17:14 -0300

    fixeado deleteComentario

--- Model/modelComentarios.php
+++ Model/modelComentarios.php
@@ -23,21 +23,19 @@ class ComentariosModel extends Model
         $query = $this->getDb()->prepare('SELECT comentario,valoracion,id_comentario from comentarios WHERE id_botin=?');
         $query->execute([$id]);
         return $query->fetchAll(PDO::FETCH_OBJ);
-       
     }
     public function GetComentario($id_comentario)
-  {
-    $sentencia = $this->db->prepare( "select * from comentarios where id_comentario=?");
-    $sentencia->execute(array($id_comentario));
-    $comment = $sentencia->fetch(PDO::FETCH_OBJ);
-    return $comment;
-  }
-
-    function DeleteComentario($id_comentario){
-        $sentencia = $this->db->prepare("DELETE FROM comentario WHERE id_comentario=30");
+    {
+        $sentencia = $this->getDb()->prepare("select * from comentarios where id_comentario=?");
         $sentencia->execute(array($id_comentario));
-        return $sentencia->rowCount();
+        $comment = $sentencia->fetch(PDO::FETCH_OBJ);
+        return $comment;
     }
 
-
+    function DeleteComentario($id_comentario)
+    {
+        $sentencia = $this->getDb()->prepare("DELETE FROM comentarios WHERE id_comentario=?");
+        $sentencia->execute([$id_comentario]);
+        return $sentencia;
+    }
 }
